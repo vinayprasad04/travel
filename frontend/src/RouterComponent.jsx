a48c3557dd0c52eb14548cab9cba3a4d8df45024
Gaurav Bajpai <gaurav.bajpai@qss-india.in>
2023-04-20 16:48:47 +0530

    Header and Routing Setup

---
+++ frontend/src/RouterComponent.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Routes, Route, Router } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Favorites from "./pages/Favorites";
+import Notifications from "./pages/Notifications";
+import Profile from "./pages/Profile";
+import Feedback from "./pages/Feedback";
+import Setting from "./pages/Setting";
+
+const RouterComponent = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/upcoming-events" element={<Favorites />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/feedback" element={<Feedback />} />
+        <Route path="/setting" element={<Setting />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+};
+
+export default RouterComponent;
