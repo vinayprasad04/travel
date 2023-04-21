618078e87b36a4181a994ce9da2e563a72620f3e
Reetu Verma <reetu.verma@qss-india.in>
2023-04-21 14:44:42 +0530

    images added

--- frontend/src/RouterComponent.jsx
+++ frontend/src/RouterComponent.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
---
+++ frontend/src/assets/img/Images.js
@@ -0,0 +1,6 @@
+const Images ={
+     img7:require("./Event-Banner.png").default,
+     img8:require("../img/Event-Banner.png").default
+
+}
+export default Images;
